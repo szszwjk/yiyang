d8eea85218551c6ceeea35790858a6c582dc3763
GitHub0225 <18636607310@163.com>
2018-09-10 10:19:01 +0800

    第四次提交

--- yiyang-manager/yiyang-manager-pojo/src/main/java/com/manager/mapper/test.java
+++ yiyang-manager/yiyang-manager-pojo/src/main/java/com/manager/mapper/test.java
@@ -2,6 +2,15 @@ package com.manager.mapper;
 
 public class test {
     private String name;
+public  String ane;
+
+    public String getAne() {
+        return ane;
+    }
+
+    public void setAne(String ane) {
+        this.ane = ane;
+    }
 
     public String getName() {
         return name;
