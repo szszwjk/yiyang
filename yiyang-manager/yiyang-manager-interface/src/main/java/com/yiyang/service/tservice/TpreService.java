be010f5297afa7ab17b3a0e6854a8ece67d7944f
GitHub0225 <18636607310@163.com>
2018-09-29 19:31:55 +0800

    0929

--- yiyang-manager/yiyang-manager-interface/src/main/java/com/yiyang/service/tservice/TpreService.java
+++ yiyang-manager/yiyang-manager-interface/src/main/java/com/yiyang/service/tservice/TpreService.java
@@ -1,8 +1,27 @@
 package com.yiyang.service.tservice;
+
 import com.yiyang.common.utils.YiyangPageResult;
+import com.yiyang.pojo.TPreservice;
+
 
 
 public interface TpreService {
-    YiyangPageResult getTpreService(int page, int rows);
+    YiyangPageResult getTpreServicedcl(int page, int rows);
+    YiyangPageResult getByUnameAndTeldcl(int page, int rows, TPreservice tPreservice);
 
-   }
+
+    YiyangPageResult getTpreServiceyjd(int page, int rows);
+    YiyangPageResult getByUnameAndTelyjd(int page, int rows, TPreservice tPreservice);
+
+    YiyangPageResult getTpreServiceywc(int page, int rows);
+    YiyangPageResult getByUnameAndTelywc(int page, int rows, TPreservice tPreservice);
+
+
+    TPreservice findByPsNumber(String psNumber);
+
+
+
+
+
+
+}
